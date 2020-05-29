03e42690fd7609e451cbacdc1baf7fc3589c0e51
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-05-30 01:05:57 +0700

    second commit

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,27 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
     
-    var currentLight = CurrentLight.off // переменная currentLight типа CurrentLight (это список/enum из файла CurrentLight.swift) - текущий цвет светофора, начальное значение выключен = off
-    let viewCornerRadius: CGFloat = 45
-    let viewAlphaOn: CGFloat = 1
-    let viewAlphaOff: CGFloat = 0.3
+    private var currentLight = CurrentLight.off // переменная currentLight типа CurrentLight (это список/enum из файла CurrentLight.swift) - текущий цвет светофора, начальное значение выключен = off
+    private let viewCornerRadius: CGFloat = 45
+    private let viewAlphaOn: CGFloat = 1
+    private let viewAlphaOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startButton.layer.cornerRadius = 15
+        startButton.layer.cornerRadius = 10
+        
         redView.layer.cornerRadius = viewCornerRadius
         yellowView.layer.cornerRadius = viewCornerRadius
         greenView.layer.cornerRadius = viewCornerRadius
+        
         redView.alpha = viewAlphaOff
         yellowView.alpha = viewAlphaOff
         greenView.alpha = viewAlphaOff
-        
     }
     
-    @IBAction func startButton(_ sender: UIButton) {
+    @IBAction func startButtonPressed() {
         
         startButton.setTitle("NEXT", for: .normal)
         
@@ -54,10 +56,5 @@ class ViewController: UIViewController {
             redView.alpha = viewAlphaOn
             currentLight = .red
         }
-        
     }
-    
-    
-    
 }
-
