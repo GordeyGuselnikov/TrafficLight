c8b9a0bdfe7c39ca192596f4717b9a6f08ea688e
Gordey Guselnikov <g.guselnikov@bi.zone>
2023-04-25 01:38:04 +0700

    add Outlets and IBAction, create func for setup UI, create logic for Button pressed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,11 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green, off
+}
 
+final class ViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    // MARK: - Private Properties
+    private var currentLight = CurrentLight.off
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func startButtonPressed() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLightView.alpha = lightIsOff
+            currentLight = .yellow
+            yellowLightView.alpha = lightIsOn
+        case .yellow:
+            yellowLightView.alpha = lightIsOff
+            currentLight = .green
+            greenLightView.alpha = lightIsOn
+        case .green:
+            greenLightView.alpha = lightIsOff
+            currentLight = .red
+            redLightView.alpha = lightIsOn
+        case .off:
+            currentLight = .red
+            redLightView.alpha = lightIsOn
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        
+        view.backgroundColor = .black
+        startButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
     }
 }
 
